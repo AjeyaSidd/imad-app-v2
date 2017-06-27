9a91f8be5f0161323f76753dddcfc2ffcefc2610
AjeyaSidd <ajeyasiddharth@gmail.com>
2017-06-27 12:59:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,20 @@
 var button = document.getElementById("counter");
-var counter = 0;
+
 button.onclick= function (){
-  //make a request to counter endpoint
+  //create a request to counter endpoint
+  var request=new XMLHttpRequest();
   
   //capture the response and store in variable
-  
-  //render the variable in the correct span
-  counter++;
-  document.getElementById("count").innerHTML=counter.toString();
+    request.onreadystatechange = function () {
+      if (request.readyState === XMLHttpRequest.DONE) {
+          if(request.status === 200 ) {
+              var counter = request.responseText;
+               document.getElementById("count").innerHTML=counter.toString();
+          }
+      }  
+    }; 
+  //make the request
+    request.open('GET','http://ajeyasidd.imad.hasura-app.io/counter','true');
+    request.send(NULL);
+ 
 };
